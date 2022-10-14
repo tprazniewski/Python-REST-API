60c9b51837d133b1e80c4d8ff03eee371b7e3de0
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-10-14 16:18:09 +0200

    Create first endpoint

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+app = Flask(__name__) 
+
+stores = [
+    {
+        "name": "MYstore",
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99
+            }
+        ]
+
+    }
+]
+
+
+@app.get('/store')
+def get_stores():
+    return {"stores": stores}
+
+@app.post('/store')
+def create_stores():
+    request_data = request.get_json()
+    new_store = {'name': request_data['name'], 'items': [] }
+    stores.append(new_store)
+    return new_store, 201
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store['name']== name:
+            new_item = {"name": request_data['name'],'price': request_data['price']}
+            store['items'].append(new_item)
+            return new_item
+    return { "message": " store not found"}, 404
